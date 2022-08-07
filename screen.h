c69a994cf90e635277ccdf5cb52735395df9b16e
Jim Hall <jhall@freedos.org>
2022-08-07 09:48:06 -0500

    Update screen.h
    Rewrite for Linux ncurses. Major code cleanup.

--- screen.h
+++ screen.h
@@ -1,7 +1,23 @@
-int init_screen(void);
-int end_screen(void);
+#include <curses.h>
 
-void title(const char *t);
-void message(const char *m);
-void error(const char *m);
-void pause(void);
+/* show binary values in a window: */
+
+void show_binary(WINDOW *win, int value);
+
+extern WINDOW *win_counter;
+extern WINDOW *win_instruction;
+extern WINDOW *win_accumulator;
+
+#define show_counter(C)     show_binary(win_counter, C)
+#define show_instruction(I) show_binary(win_instruction, I)
+#define show_accumulator(A) show_binary(win_accumulator, A)
+
+/* message display functions: */
+
+void print_title(const char *title);
+void print_message(const char *msg);
+
+/* screen init functions: */
+
+void init_screen(void);
+void end_screen(void);
