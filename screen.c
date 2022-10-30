f58452d4079b2330c5878b916d62d38e4aaa661d
Jim Hall <jhall@freedos.org>
2022-10-30 16:11:03 -0500

    Screen updates in DOS graphical mode
    Complete rewrite of screen functions using DOS graphical mode

--- screen.c
+++ screen.c
@@ -1,135 +1,190 @@
-#include <curses.h> /* ncurses */
 #include <string.h> /* strlen */
-
-#include "bit.h"
-
-WINDOW *win_counter;
-WINDOW *win_instruction;
-WINDOW *win_accumulator;
-
-void
-show_binary(WINDOW *win, int value)
-{
-  wmove(win, 2, 2);
-
-  for (int bit = 7; bit >= 0; bit--) {
-    waddch(win, ((1<<bit) & value) ? BIT_1 : BIT_0 );
-  }
-  wrefresh(win);
-}
+#include <conio.h>
+#include <graph.h>
 
 void
 print_title(const char *title)
 {
-  attron(A_BOLD);
-  
-  mvaddstr(0, (COLS - strlen(title)) / 2, title);
-  refresh();
+    _setcolor(11); /* br cyan */
 
-  attroff(A_BOLD);
+    /* approx center the title: the screen is 640x480, so center is
+	320. text is 8x8, so half the string length is:
+	strlen(title)*8/2 = strlen(title)*4 */
+
+    _moveto(320 - strlen(title)*4, 1);
+    _outgtext(title);
 }
 
 void
-print_message(const char *msg)
+print_message(const char *message)
 {
-  attron(A_REVERSE);
+    /* draw a box .. this also clears the message area */
 
-  mvhline(LINES-1, 0, ' ', COLS);
+    _setcolor(1); /* blue */
+    _rectangle(_GFILLINTERIOR, 0, 470, 639, 479);
 
-  mvaddstr(LINES-1, 0, msg);
-  refresh();
+    /* print the text */
 
-  attroff(A_REVERSE);
+    _setcolor(7); /* white */
+    _moveto(4, 471);
+    _outgtext(message);
 }
 
 void
-print_instr(void)
+pause(const char *msg)
 {
-  int row, col;
+    if (msg == NULL) {
+	print_message("Press any key...");
+    }
+    else {
+	print_message(msg);
+    }
 
-  row = LINES/5;
-  col = COLS - 9 /* instr */ - 7 /* mnem */;
+    /* getch() returns zero if an extended key. call getch() again to
+	get the extended value. in this case, we're just waiting for a
+	key, so call again anyway if getch() is zero, to clear the ext
+	key value. */
 
-  attron(A_DIM);
-
-  mvaddstr(row++, col, "........ STOP");
-
-  mvaddstr(row++, col, ".......* RIGHT");
-  mvaddstr(row++, col, "......*. LEFT");
-
-  mvaddstr(row++, col, "....**** NOT");
-  mvaddstr(row++, col, "...*...* AND");
-  mvaddstr(row++, col, "...*..*. OR");
-  mvaddstr(row++, col, "...*..** XOR");
-
-  mvaddstr(row++, col, "...*.*.. LOAD");
-  mvaddstr(row++, col, "...*.*.* STORE");
-
-  mvaddstr(row++, col, "...*.**. ADD");
-  mvaddstr(row++, col, "...*.*** SUB");
-
-  mvaddstr(row++, col, "...**... GOTO");
-  mvaddstr(row++, col, "...**..* IFZERO");
-
-  mvaddstr(row++, col, "*....... NOP");
-
-  attroff(A_DIM);
+    if (getch() == 0) {
+	getch();
+    }
 }
 
-void
-init_screen(void)
+int
+init_screen()
 {
-  /* init the screen */
+    int lines;
 
-  initscr();
-  cbreak();
-  noecho();
+    /* set up the video mode: 640x480 @ 16 colors */
 
-  /* title */
+    lines = _setvideomode(_VRES16COLOR);
 
-  print_title("Toy CPU Simulator");
+    if (lines == 0) {
+	return 0;
+    }
 
-  /* windows */
+    /* set up the user interface */
 
-  win_counter = newwin(5, 12, LINES/5, COLS/4);
-  box(win_counter, 0, 0);
-  mvwaddstr(win_counter, 0, 1, "count");
-  wrefresh(win_counter);
+    /* two-tone border around the screen, with 20px border and 10px
+	reveal. add an extra 200px x 20px space on the upper-left
+	of the screen for the program title, and 100px extra space
+	on the bottom for hinting. */
 
-  win_instruction = newwin(5, 12, LINES/5, COLS/2);
-  box(win_instruction, 0, 0);
-  mvwaddstr(win_instruction, 0, 1, "instr");
-  wrefresh(win_instruction);
+    _setcolor(1); /* blue */
 
-  win_accumulator = newwin(5, 12, LINES/2, COLS/2);
-  box(win_accumulator, 0, 0);
-  mvwaddstr(win_accumulator, 0, 1, "accum");
-  wrefresh(win_accumulator);
+#define XRES 639
+#define YRES 479
 
-  /* init displays */
+    /* top */
+    _rectangle(_GFILLINTERIOR, 0, 0, XRES, 20);
+    /* left */
+    _rectangle(_GFILLINTERIOR, 0, 0, 20, YRES);
+    /* right */
+    _rectangle(_GFILLINTERIOR, XRES-20, 0, XRES, YRES);
+    /* bottom */
+    _rectangle(_GFILLINTERIOR, 0, YRES-100-10, XRES, YRES);
+    /* title */
+    _rectangle(_GFILLINTERIOR, 0, 0, 200+10, 20+10);
+    /* rounded corner (title) */
+    _ellipse(_GFILLINTERIOR, 200+10-20, 10, 200+10+20, 10+20);
 
-  /*
-  show_binary(win_counter, 0);
-  show_binary(win_instruction, 0);
-  show_binary(win_accumulator, 0);
-  */
+    _setcolor(3); /* cyan */
 
-  /* help */
+    /* top */
+    _rectangle(_GFILLINTERIOR, 0, 0, XRES, 10);
+    /* left */
+    _rectangle(_GFILLINTERIOR, 0, 0, 10, YRES);
+    /* right */
+    _rectangle(_GFILLINTERIOR, XRES-10, 0, XRES, YRES);
+    /* bottom */
+    _rectangle(_GFILLINTERIOR, 0, YRES-100, XRES, YRES);
+    /* title */
+    _rectangle(_GFILLINTERIOR, 0, 0, 200, 20);
+    /* rounded corner (title) */
+    _ellipse(_GFILLINTERIOR, 200-20, 0, 200+20, 20);
 
-#if defined(USE_HELP)
-  print_instr();
-#endif
-}
+    /* print title */
 
-void
-end_screen(void)
-{
-  print_message("press any key to quit");
-  getch();
+    _setcolor(11); /* br cyan */
+    _moveto(46, 6);
+    _outgtext("Toy CPU Simulator");
 
-  delwin(win_counter);
-  delwin(win_instruction);
-  delwin(win_accumulator);
+    /* print hints */
 
-  endwin();
+    /*
+	\030 = up, \031 = down, \032 = right, \033 = left
+	\022 = up/down,  \035 = left/right
+	\263 = vert bar, \304 = horiz bar
+	\371 = bullet,   \372 = middot
+    */
+
+    _moveto(130, YRES-80);
+    _outgtext("Input mode: \022 \372 counter \263 Enter \372 edit \263 R \372 run \263 Q \372 quit");
+
+    _moveto(130, YRES-60);
+    _outgtext("Edit mode: \035 \372 bit \263 Space \372 flip \263 Enter \372 done");
+
+    /* print labels */
+
+    _setcolor(8); /* br white */
+
+    _moveto(54, 90);
+    _outgtext("Counter");
+
+    _moveto(330, 90);
+    _outgtext("Instruction");
+
+    _moveto(330, 190);
+    _outgtext("Accumulator");
+
+    /* LEDs are 32px wide, text is 8x8, so three letters plus a space
+	is 32px. offset by 4px to center the text over each LED. */
+
+    _moveto(334, 290);
+    _outgtext("INI HLT ERR ABT RUN EDT INP PWR");
+
+    /* print cheat sheet */
+
+    /*
+	\371 = bullet,   \372 = middot
+	\376 = square
+    */
+
+    _setcolor(7); /* white */
+    _rectangle(_GFILLINTERIOR, 50, 200, 230, 356); /* "paper" */
+
+    _setcolor(8); /* br black */
+    _rectangle(_GFILLINTERIOR, 125, 195, 150, 205); /* "tape" */
+
+    _setcolor(0); /* black */
+    _moveto(60, 210);
+    _outgtext("\372\372\372\372\372\372\372\372 STOP");
+    _moveto(60, 220);
+    _outgtext("\372\372\372\372\372\372\372\376 RIGHT");
+    _moveto(60, 230);
+    _outgtext("\372\372\372\372\372\372\376\372 LEFT");
+    _moveto(60, 240);
+    _outgtext("\372\372\372\372\376\376\376\376 NOT");
+    _moveto(60, 250);
+    _outgtext("\372\372\372\376\372\372\372\376 AND addr");
+    _moveto(60, 260);
+    _outgtext("\372\372\372\376\372\372\376\372 OR addr");
+    _moveto(60, 270);
+    _outgtext("\372\372\372\376\372\372\376\376 XOR addr");
+    _moveto(60, 280);
+    _outgtext("\372\372\372\376\372\376\372\372 LOAD addr");
+    _moveto(60, 290);
+    _outgtext("\372\372\372\376\372\376\372\376 STORE addr");
+    _moveto(60, 300);
+    _outgtext("\372\372\372\376\372\376\376\372 ADD addr");
+    _moveto(60, 310);
+    _outgtext("\372\372\372\376\372\376\376\376 SUB addr");
+    _moveto(60, 320);
+    _outgtext("\372\372\372\376\376\372\372\372 GOTO addr");
+    _moveto(60, 330);
+    _outgtext("\372\372\372\376\376\372\372\376 IFZERO addr");
+    _moveto(60, 340);
+    _outgtext("\376\372\372\372\372\372\372\372 NOP");
+
+    return lines;
 }
--- screen.h
+++ screen.h
@@ -1,23 +1,18 @@
-#include <curses.h>
+#if !defined(SCREEN_H)
+#define SCREEN_H
 
-/* show binary values in a window: */
+void
+print_title(const char *title);
 
-void show_binary(WINDOW *win, int value);
+void
+print_message(const char *message);
 
-extern WINDOW *win_counter;
-extern WINDOW *win_instruction;
-extern WINDOW *win_accumulator;
+void
+pause(const char *msg);
 
-#define show_counter(C)     show_binary(win_counter, C)
-#define show_instruction(I) show_binary(win_instruction, I)
-#define show_accumulator(A) show_binary(win_accumulator, A)
+int
+init_screen();
 
-/* message display functions: */
+#define end_screen() (_setvideomode(_DEFAULTMODE))
 
-void print_title(const char *title);
-void print_message(const char *msg);
-
-/* screen init functions: */
-
-void init_screen(void);
-void end_screen(void);
+#endif
