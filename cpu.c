9f9538553b810ee2cc387b06a82d5171fcc3df14
Jim Hall <jhall@freedos.org>
2022-10-30 16:13:08 -0500

    New CPU handling
    Completely rewritten CPU handling using new DOS graphical mode display

--- cpu.c
+++ cpu.c
@@ -1,158 +1,175 @@
-#if !defined(DEBUG)
-#include <unistd.h> /* sleep */
-#endif
+#include <conio.h> /* kbhit, getch */
 
+#include "binary.h"
 #include "screen.h"
-#include "cpuinstr.h"
+
+#include "cpu.h"
+#include "opcodes.h"
 
 int Accumulator;
 
-/* functions to manage the CPU */
-
 int
-cpu_exec(int *mem, int count)
+next_instruction(int counter)
 {
-  int next;
-  int addr;
-  
-  /* execute a single instruction from memory. implements a simple
-     fetch/decode/execute model. fetch is the reference in the mem
-     array. the decode is via a switch block. execute is each case in
-     the switch block. */
+    /* the next instruction to execute */
 
-  show_counter(count);
-  show_instruction(mem[count]);
-
-#if defined(DEBUG)
-  getch();
-#else
-  sleep(1);
-#endif
-
-  /* returns the counter to the next instruction to execute. */
-
-  next = (count<255 ? count+1 : 0);
-
-  /* some operations require an address from the next program
-     instruction. these instructions have the "fetch" bit set. */
-
-  if (mem[count] & FETCH_BIT) {
-    /* update the display */
-    
-    show_counter(next);
-    show_instruction(mem[next]);
-
-#if defined(DEBUG)
-    getch();
-#else
-    sleep(1);
-#endif
-
-    /* advance the next counter */
-    
-    addr = mem[next];
-    next = (next<255 ? next+1 : 0);
-  }
-
-  /* the switch block recognizes each CPU instruction as machine
-     code. technically there is a NOP instruction, but it's not listed
-     explicitly in the switch block because NOP is always ignored. any
-     non-recognized instruction is effectively a NOP, but be careful
-     for any unrecognized instructions that happen to have the "fetch"
-     bit set, these cause an (unwanted) separate fetch of data. */
-
-  switch(mem[count]) {
-  case CPU_STOP:
-    return -1;
-    
-  case CPU_RIGHT:
-    Accumulator = Accumulator>>1;
-    break;
-    
-  case CPU_LEFT:
-    Accumulator = Accumulator<<1;
-    break;
-    
-  case CPU_NOT:
-    Accumulator = ~Accumulator;
-    break;
-    
-  case CPU_AND: /* binary AND the Accumulator with value at addr */
-    Accumulator = Accumulator & mem[addr];
-    break;
-    
-  case CPU_OR: /* binary OR the Accumulator with value at addr */
-    Accumulator = Accumulator | mem[addr];
-    break;
-    
-  case CPU_XOR: /* binary XOR the Accumulator with value at addr */
-    Accumulator = Accumulator ^ mem[addr];
-    break;
-    
-  case CPU_LOAD: /* load Accumulator with value at addr */
-    Accumulator = mem[addr];
-    break;
-    
-  case CPU_STORE: /* save Accumulator to addr */
-    mem[addr] = Accumulator;
-    break;
-    
-  case CPU_ADD: /* add value at addr to Accumulator */
-    Accumulator += mem[addr];
-    if (Accumulator > 255) {
-      /* keep Accumulator in bounds 0 to 255 : 255 + 1 = 0 */
-      Accumulator = Accumulator - 256;
+    if (counter < 255) {
+	return counter+1;
     }
-    break;
-    
-  case CPU_SUB: /* subtract value at addr from Accumulator */
-    Accumulator -= mem[addr];
-    if (Accumulator < 0) {
-      /* keep Accumulator in bounds 0 to 255 : 0 - 1 = 255 */
-      Accumulator = Accumulator + 256;
-    }
-    break;
-    
-  case CPU_GOTO: /* set next instr to addr */
-    next = addr;
-    
-  case CPU_IFZERO: /* if Accumulator is zero, set next instr to addr */
-    if (Accumulator == 0) {
-      next = addr;
-    }
-  }
 
-  /* update the display */
+    /* else, loop back around to zero (keep in 0 to 255: 255+1=0) */
 
-  show_accumulator(Accumulator);
-
-#if defined(DEBUG)
-  getch();
-#else
-  sleep(1);
-#endif
-
-  return next;
+    draw_status(STATUS_ERR | STATUS_RUN); /* overflow */
+    return 0;
 }
 
 int
-run_program(int *mem)
+run_instruction(int *mem, int counter)
 {
-  /* execute a program from memory, starting at count=0 */
+    int next, addr;
 
-  int count = 0;
+    /* execute a single instruction from memory. implements a single
+	fetch/decode/execute model. fetch is the reference from mem,
+	decode is via the switch block, execute is each case in the
+	switch block. */
 
-  print_message("run mode . . .");
-  
-  Accumulator = 0;
-  show_accumulator(Accumulator);
+    draw_count(counter);
+    draw_instr(mem[counter]);
+    tick();
 
-  do {
-    count = cpu_exec(mem, count);
-  } while (count >= 0);
+    next = next_instruction(counter);
 
-  print_message("done | press any key to enter program mode");
-  getch();
+    /* some operations require an address from the next program
+	instruction. these instructions have a "fetch" bit set. */
 
-  return Accumulator;
+    if (mem[counter] & FETCH_BIT) {
+	draw_count(next);
+	draw_instr(mem[next]);
+	tick();
+
+	addr = mem[next]; /* set the address value */
+	next = next_instruction(next); /* set the next instruction */
+    }
+
+    /* the switch block recognizes each CPU instruction as a CPU
+	opcode, written in machine language (binary).
+
+	technically, there is a NOP instruction, but it's not
+	listed here because any unrecognized opcode is considered
+	a NOP instruction. BE CAREFUL OF USING UNRECOGNIZED
+	OPCODES WITH THE FETCH BIT SET, these will also advance
+	the next counter, and will result in a programming bug. */
+
+    switch(mem[counter]) {
+    case CPU_STOP:
+	return -1;
+
+    case CPU_RIGHT:
+	Accumulator = Accumulator>>1;
+	break;
+
+    case CPU_LEFT:
+	Accumulator = Accumulator<<1;
+	break;
+
+    case CPU_NOT:
+	Accumulator = ~Accumulator;
+	break;
+
+    case CPU_AND:
+	Accumulator = Accumulator & mem[addr];
+	break;
+
+    case CPU_OR:
+	Accumulator = Accumulator | mem[addr];
+	break;
+
+    case CPU_XOR:
+	Accumulator = Accumulator ^ mem[addr];
+	break;
+
+    case CPU_LOAD:
+	Accumulator = mem[addr];
+	break;
+
+    case CPU_STORE:
+	mem[addr] = Accumulator;
+	break;
+
+    case CPU_ADD:
+	Accumulator += mem[addr];
+
+	if (Accumulator > 255) {
+	    Accumulator -= 256; /* keep accum in bounds 0 to 255: 255+1=0 */
+	    draw_status(STATUS_ERR | STATUS_RUN); /* overflow */
+	}
+	break;
+
+    case CPU_SUB:
+	Accumulator -= mem[addr];
+
+	if (Accumulator < 0) {
+	    Accumulator += 256; /* keep accum in bounds 0 to 255: 0-1=255 */
+	    draw_status(STATUS_ERR | STATUS_RUN); /* underflow */
+	}
+	break;
+
+    case CPU_GOTO:
+	next = addr;
+	break;
+
+    case CPU_IFZERO:
+	if (Accumulator == 0) {
+	    next = addr;
+	}
+    }
+
+    /* update the display */
+
+    draw_accum(Accumulator);
+    tick();
+
+    return next;
+}
+
+int
+run_program(int *mem, int start)
+{
+    int key;
+    int counter;
+
+    draw_status(STATUS_RUN); /* run mode */
+
+    /* run the program */
+
+    Accumulator = 0;
+    draw_accum(Accumulator);
+
+    counter = start;
+
+    do {
+	counter = run_instruction(mem, counter);
+
+	/* do we need to abort? */
+
+	if (kbhit()) {
+	    key = getch();
+
+	    if (key==0) {
+		/* getch returns zero if extended key .. call getch
+		    again to clear it */
+		getch();
+	    }
+	    else if (key == 27) { /* ESC key */
+		draw_status(STATUS_ABT | STATUS_RUN);
+		tick();
+
+		return Accumulator;
+	    }
+	}
+    } while (counter >= 0);
+
+    tick();
+
+    return Accumulator;
 }
--- cpu.h
+++ cpu.h
@@ -1,1 +1,18 @@
-int run_program(int *mem);
+#if !defined(CPU_H)
+#define CPU_H
+
+int
+run_program(int *mem, int start);
+
+#include <i86.h> /* delay */
+
+/* half a second (500ms) */
+/* #define tick() (delay(500)) */
+
+/* one second (1000ms) */
+#define tick() (delay(1000))
+
+/* two seconds (2000ms) */
+/* #define tick() (delay(2000)) */
+
+#endif
